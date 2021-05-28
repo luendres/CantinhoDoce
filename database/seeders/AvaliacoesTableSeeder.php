3aa12401390f7d9d3f2d71f4f36acf1b301e5872
mariasantos27 <iscac17888@alumni.iscac.pt>
2021-05-28 23:23:22 +0100

    avaliações

--- database/seeders/AvaliacoesTableSeeder.php
+++ database/seeders/AvaliacoesTableSeeder.php
@@ -18,6 +18,19 @@ class AvaliacoesTableSeeder extends Seeder
 
         \DB::table('avaliacoes')->delete();
         
+        \DB::table('avaliacoes')->insert(array (
+            0 => 
+            array (
+                'id' => 1,
+                'nome' => 'Luisa',
+                'produto_id' => 21,
+                'nota' => 5,
+                'avaliacao' => 'ótimo produto',
+                'estado' => 1,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+        ));
         
         
     }
