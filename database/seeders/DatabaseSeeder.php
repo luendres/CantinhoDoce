9680088872f8ee159669d618e83f5cc172dac7c6
mariasantos27 <iscac17888@alumni.iscac.pt>
2021-05-28 17:27:48 +0100

    admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,7 +14,15 @@ class DatabaseSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
+    {   
+        User::create([
+            'nome' => 'Nome do admin',
+            'email' => 'admin2@cantinhodoce.com',
+            'password' => Hash::make('12345678'),
+            'is_admin' => 1,
+            'email_verified_at' => now()->toDateString()
+        ]);
+
         // \App\Models\User::factory(10)->create();
     }
 }
